7e73f871fc52eb1f6192048b631f7664d11369f6
omer1abay <abayomer1905@gmail.com>
2025-01-07 01:29:18 +0300

    proxy errors solved

--- SagaPattern.AppHost/Program.cs
+++ SagaPattern.AppHost/Program.cs
@@ -4,18 +4,22 @@ var rabbitmq = builder.AddRabbitMQ("messaging");
 
 builder
     .AddProject<Projects.SagaPattern_Orchestration_OrchestratorService>("sagapattern-orchestration-orchestratorservice")
-    .WithReference(rabbitmq);
+    .WithReference(rabbitmq)
+    .WaitFor(rabbitmq);
 
 builder
     .AddProject<Projects.SagaPattern_Orchestration_OrderService>("sagapattern-orchestration-orderservice")
-    .WithReference(rabbitmq); 
+    .WithReference(rabbitmq)
+    .WaitFor(rabbitmq);
 
 builder
     .AddProject<Projects.SagaPattern_Orchestration_PaymentService>("sagapattern-orchestration-paymentservice")
-    .WithReference(rabbitmq); 
+    .WithReference(rabbitmq)
+    .WaitFor(rabbitmq);
 
 builder
     .AddProject<Projects.SagaPattern_Orchestration_ProductService>("sagapattern-orchestration-productservice")
-    .WithReference(rabbitmq); 
+    .WithReference(rabbitmq)
+    .WaitFor(rabbitmq);
 
 builder.Build().Run();
